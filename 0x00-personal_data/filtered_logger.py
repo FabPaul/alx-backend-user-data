67287af26eb3bb19e9f88364bef90a0ac3400f52
FabPaul <paulfopa0@gmail.com>
2024-02-11 13:22:05 +0100

    added return type for the function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,7 +7,7 @@ from typing import List
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: str):
+                 separator: str) -> str:
     """Returns the log messaged obfuscated"""
     for field in fields:
         message = re.sub(f'{field}=.*?{separator}',

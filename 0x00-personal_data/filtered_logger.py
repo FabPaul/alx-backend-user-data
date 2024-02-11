5c8b255045c80503b5da1134664ea4281200cf03
FabPaul <paulfopa0@gmail.com>
2024-02-11 14:25:26 +0100

    Added create logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,6 +7,8 @@ from typing import List
 import logging
 
 
+PII_FIELDS = ['name', 'email', 'phone', 'ssn', 'password']
+
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """Returns the log messaged obfuscated"""
@@ -35,3 +37,15 @@ class RedactingFormatter(logging.Formatter):
                                   record.getMessage(), self.SEPARATOR)
         filters = super(RedactingFormatter, self).format(record)
         return filters
+
+
+def get_logger() -> logging.Logger:
+    """Logger funtion"""
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    logger.addHandler(stream_handler)
+
+    return logger

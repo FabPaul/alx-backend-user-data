21e8695e4ac16f4aa33f90b3cf4a973241311092
FabPaul <paulfopa0@gmail.com>
2024-02-11 16:48:01 +0100

    Added check for valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,3 +10,9 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Checks valid password"""
+    password_bytes = password.encode('utf-8')
+    return bcrypt.checkpw(password_bytes, hashed_password)

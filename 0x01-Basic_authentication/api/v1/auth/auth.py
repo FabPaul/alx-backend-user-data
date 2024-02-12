7614e7636e94d07c8d496f7c9375746e8c90fd93
FabPaul <paulfopa0@gmail.com>
2024-02-12 17:08:12 +0100

    Defined routes that don't need authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,7 +10,18 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Module for required paths"""
-        return False
+        if path is None:
+            return True
+
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        if path in excluded_paths:
+            return False
+
+        path = path.rstrip('/') + '/'
+
+        return path not in excluded_paths
 
     def authorization_header(self, request=None) -> str:
         """Authorized headers"""

dd6f2568a29789f0b91b49ec844f21b36f6788a0
FabPaul <paulfopa0@gmail.com>
2024-02-13 10:55:24 +0100

    Added request validation

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,6 +12,14 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+
+
+AUTH_TYPE = os.getenv('AUTH_TYPE')
+
+if AUTH_TYPE == 'auth':
+    from api.v1.auth.auth import Auth
+    auth = Auth()
 
 
 @app.errorhandler(404)
@@ -31,6 +39,24 @@ def unauthorized(error) -> str:
 def forbidden(error) -> str:
     """ Forbidden"""
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request():
+    """Method to handle before request"""
+    if auth is None:
+        return
+
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
+    if request.path not in excluded_paths:
+        if not auth.require_auth(request.path, excluded_paths):
+            return
+        if auth.authorization_header(request) is None:
+            abort(401, description="Unauthorized")
+        if auth.current_user(request) is None:
+            abort(403, description="Forbidden")
 
 
 if __name__ == "__main__":
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -25,7 +25,13 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """Authorized headers"""
-        return None
+        if request is None:
+            return None
+
+        if 'Authorization' not in request.headers:
+            return None
+
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Current user"""

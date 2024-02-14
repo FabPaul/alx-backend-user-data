3f93941de303f024feed6df756f73718bb303a8c
FabPaul <paulfopa0@gmail.com>
2024-02-14 19:10:38 +0100

    Added session id for identifying a user (task6)

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,7 @@
 
 from api.v1.auth.auth import Auth
 from uuid import uuid4
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -31,3 +32,11 @@ class SessionAuth(Auth):
 
         userId = self.user_id_by_session_id.get(session_id)
         return userId
+
+    def current_user(self, request=None):
+        """Returns a user instance based on a cookie value"""
+        session_cookie = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_cookie)
+        user = User.get(user_id)
+
+        return user

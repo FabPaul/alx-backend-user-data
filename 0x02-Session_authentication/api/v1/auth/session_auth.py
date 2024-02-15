cd951a63db78bb83c98c201b64be52e7854f11f3
FabPaul <paulfopa0@gmail.com>
2024-02-15 12:30:40 +0100

    Added logout route

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -40,3 +40,16 @@ class SessionAuth(Auth):
         user = User.get(user_id)
 
         return user
+
+    def destroy_session(self, request=None):
+        """Deletes the user session/logout"""
+        if request is None:
+            return False
+        session_id_cookie = self.session_cookie(request)
+        if session_id_cookie is None:
+            return False
+        user_id = self.user_id_for_session_id(session_id_cookie)
+        if user_id is None:
+            return False
+        del(self.user_id_by_session_id[session_id_cookie])
+        return True
--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -34,3 +34,13 @@ def session_auth():
             return response
 
     return jsonify({"error": "wrong password"}), 401
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
+def session_logout():
+    """Logout/delete auth route"""
+    from api.v1.app import auth
+    if not auth.destroy_session(request):
+        abort(404)
+    return jsonify({}), 200

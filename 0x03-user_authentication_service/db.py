47121d16f688fd414acba23354abd10080a2d4af
FabPaul <paulfopa0@gmail.com>
2024-02-20 12:53:15 +0100

    Added find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,6 +6,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -38,3 +40,15 @@ class DB:
         self.__session.commit()
 
         return user_created
+
+    def find_user_by(self, **kwargs):
+        """FIids a user by arbitrary keyword arguments"""
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+            if user is None:
+                raise NoResultFound
+            return user
+        except NoResultFound as e:
+            raise e
+        except InvalidRequestError as e:
+            raise InvalidRequestError from e

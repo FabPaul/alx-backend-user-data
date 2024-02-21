f92515f3c437250c04754765311556bd64cf0e1a
FabPaul <paulfopa0@gmail.com>
2024-02-21 12:52:25 +0100

    status code is 403, not 400. typo

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -74,7 +74,7 @@ def get_reset_password_token():
         reset_token = AUTH.get_reset_password_token(email)
         return jsonify({"email": email, "reset_token": reset_token}), 200
     except Exception:
-        abort(400)
+        abort(403)
 
 
 if __name__ == "__main__":
